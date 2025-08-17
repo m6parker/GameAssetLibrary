aabbb04e5c78873c38eab19b01156bd7058755da
Mary Parker <98114662+m6parker@users.noreply.github.com>
2025-08-17 14:00:47 -0400

    added ui elemetns

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,3 +1,23 @@
 const uiFiles = [
-    'pixel_pond_ui.png'
+    'pixel_pond_ui.png',
+
+    'back_button.png',
+    'controls_button.png',
+    'exit_button.png',
+    'load_button.png',
+    'start_button.png',
+    'new_game_button.png',
+    'yes_button.png',
+    'no_button.png',
+    'play_button.png',
+    'quit_button.png',
+
+    'canvas_character_select.png',
+    'canvas_hotbar.png',
+    'canvas_large.png',
+    'canvas_small.png',
+    'canvas_news.png',
+
+    'controls_buttons.png',
+    'highlight_item.png',
 ];
